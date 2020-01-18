786ee4f74ba0bbce1d0c5fd97d64e859c344654a
Inderpal Singh <inderpal2406@gmail.com>
2020-01-18 20:33:58 +0530

    Add test stage files for java app pipeline

--- jenkins_ansible_docker/jenkins/test/mvn.sh
+++ jenkins_ansible_docker/jenkins/test/mvn.sh
@@ -3,7 +3,7 @@
 # script to build jar file using maven image and docker
 
 echo "**********************************************"
-echo "************** Building JAR ******************"
+echo "************** Testing JAR *******************"
 echo "**********************************************"
 
-docker run --name maven --rm -v $PWD/java_app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --name maven_test --rm -v $PWD/java_app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
