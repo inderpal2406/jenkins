f3092a66c075e426d411fa223ca329bced05e0b1
Inderpal Singh <inderpal2406@gmail.com>
2020-01-18 20:46:02 +0530

    Update jenkins/test/mvn.sh

--- jenkins_ansible_docker/jenkins/test/mvn.sh
+++ jenkins_ansible_docker/jenkins/test/mvn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# script to build jar file using maven image and docker
+# script to test using maven image and docker
 
 echo "**********************************************"
-echo "************** Testing JAR *******************"
+echo "*************** Testing **********************"
 echo "**********************************************"
 
 docker run --name maven_test --rm -v $PWD/java_app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
