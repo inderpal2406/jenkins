872f3aca5b192e9a3b367e36c04c422109acfe96
Inderpal Singh <inderpal2406@gmail.com>
2020-01-19 09:04:06 +0530

    Delete jenkins/test folder

--- jenkins_ansible_docker/jenkins/test/mvn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# script to test using maven image and docker
-
-echo "**********************************************"
-echo "*************** Testing **********************"
-echo "**********************************************"
-
-docker run --name maven_test --rm -v $PWD/java_app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
