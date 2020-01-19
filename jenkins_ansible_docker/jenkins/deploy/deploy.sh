9919dd25cb4d9894408f6a17999a651ebc43d270
Inderpal Singh <inderpal2406@gmail.com>
2020-01-19 18:20:39 +0530

    Update pvt ssh key path in deploy.sh

--- jenkins_ansible_docker/jenkins/deploy/deploy.sh
+++ jenkins_ansible_docker/jenkins/deploy/deploy.sh
@@ -12,11 +12,11 @@ echo
 # copy var file and deployment script to remote machine
 
 echo "********** Copying files to remote machine **********"
-scp -i ./ssh_keys/prod-user-key /tmp/.auth prod-user@18.138.248.253:/tmp/
-scp -i ./ssh_keys/prod-user-key ./jenkins/deploy/publish.sh prod-user@18.138.248.253:/tmp/
+scp -i /tmp/prod-user-key /tmp/.auth prod-user@18.138.248.253:/tmp/
+scp -i /tmp/prod-user-key ./jenkins/deploy/publish.sh prod-user@18.138.248.253:/tmp/
 echo
 
 # ssh to remote machine and execute deployment script
 echo "***** SSH to remote machine and execute deployment script to make the deployment *****"
-ssh -i ./ssh_keys/prod-user-key prod-user@18.138.248.253 "chmod +x /tmp/publish.sh; /tmp/publish.sh"
+ssh -i /tmp/prod-user-key prod-user@18.138.248.253 "chmod +x /tmp/publish.sh; /tmp/publish.sh"
 
